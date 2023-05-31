fe1e8ca46a856d0f8833753154d96498ae1e20f0
amrxt1 <amrxt11@outlook.com>
2023-05-31 14:16:49 +0530

    Make a redirect_to root_url call in case RecordNotFound

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,10 +3,14 @@ require 'flickr'
 class StaticPagesController < ApplicationController
     def index
         if params[:id]
+            begin 
             flickr = Flickr.new( Figaro.env.flickr_key, Figaro.env.flickr_secret )
             photos_list = flickr.people.getPublicPhotos(user_id: params[:id])
 
             @photos = photos_list.map { |photo| Flickr.url(photo) } 
+            rescue
+                redirect_to root_url
+            end
         end
     end
 end
